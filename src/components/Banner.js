4535e2181f098daf988b7d1e3856a03f9ec1891b
TheGreatestHacker <bryancruz457@hotmail.com>
2021-06-07 18:50:03 -0500

    Add movie rows to home screen

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -22,7 +22,7 @@ function Banner() {
         fetchData();
     }, []);
 
-    console.log(movie);
+    //console.log(movie);
 
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n - 1) + '...' : string;
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './HomeScreen.css';
 import Nav from './Nav';
 import Banner from './Banner';
+import requests from '../Requests';
+import Row from './Row';
 
 function HomeScreen() {
     return (
@@ -12,8 +14,18 @@ function HomeScreen() {
             {/* Banner */}
             <Banner />
 
-            {/* Row */}
+            {/* Rows */}
+            <Row 
+                title='NETFLIX ORIGINALS'
+                fetchUrl={requests.fetchNetflixOriginals}
+                isLargeRow
+            />
+            <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+            <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+            <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
             
+
+
         </div>
     )
 }
