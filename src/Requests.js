26cd17f22a4887a71485cac661465c8bbb78f146
TheGreatestHacker <bryancruz457@hotmail.com>
2021-06-06 23:01:09 -0500

    Add homescreen banner component + axios

---
+++ src/Requests.js
@@ -0,0 +1,19 @@
+const API_KEY = process.env.REACT_APP_API_KEY
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorroMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchMysteryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchSciFi: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+    fetchWestern: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+    fetchAnimation: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+    fetchTV: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+}
+
+export default requests;
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HomeScreen.css';
 import Nav from './Nav';
+import Banner from './Banner';
 
 function HomeScreen() {
     return (
@@ -9,6 +10,7 @@ function HomeScreen() {
             <Nav />
             
             {/* Banner */}
+            <Banner />
 
             {/* Row */}
             
